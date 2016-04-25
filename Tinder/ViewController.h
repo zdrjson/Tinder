1322066b507c1b380bb46225274e84f21dca9b78
张德荣 <zdrjson@sina.com>
2016-04-25 18:55:25 +0800

    build Tinder project

---
+++ Tinder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tinder
+//
+//  Created by 张德荣 on 16/4/25.
+//  Copyright © 2016年 JsonZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tinder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tinder
+//
+//  Created by 张德荣 on 16/4/25.
+//  Copyright © 2016年 JsonZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
