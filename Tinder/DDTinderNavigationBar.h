dc3b7b8f3bafc0fdf92e6e42c9ce82fef3e236d4
张德荣 <zdrjson@sina.com>
2016-04-26 23:50:51 +0800

    define navigationBar api and property

--- Tinder/DDTinderNavigationBar.h
+++ Tinder/DDTinderNavigationBar.h
@@ -7,7 +7,25 @@
 //
 
 #import <UIKit/UIKit.h>
-
+@class DDTinderNavigaitonController;
 @interface DDTinderNavigationBar : UINavigationBar
+
+@property (nonatomic, strong) NSArray *itemViews;
+
+@property (nonatomic, assign) NSInteger currentPage;
+
+@property (nonatomic, assign) CGPoint contentOffSet;
+
+@property (nonatomic, strong) DDTinderNavigaitonController *navigationController;
+
+- (void)reloadData;
+
+@end
+
+@protocol DDTinderNavigationBarItem <NSObject>
+
+@optional
+
+- (void)updateViewWithRatio:(CGFloat)ratio;
 
 @end
