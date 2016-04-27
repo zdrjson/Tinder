144dd87f0021cfaf7d419c9e74575eb488953525
张德荣 <zdrjson@sina.com>
2016-04-27 22:15:07 +0800

    perfect reloadData method

--- Tinder/DDTinderNavigationBar.m
+++ Tinder/DDTinderNavigationBar.m
@@ -8,6 +8,11 @@
 
 #import "DDTinderNavigationBar.h"
 #import "DDTinderNavigaitonController.h"
+
+#define WIDTH self.bounds.size.width
+#define IMAGESIZE 30
+#define Y_POSITION 28
+
 @interface DDTinderNavigationBar ()
 
 @end
@@ -18,13 +23,48 @@
     if (!self.items.count) {
         return;
     }
-    [self.itemViews enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+    [self.itemViews enumerateObjectsUsingBlock:^(UIView<DDTinderNavigationBarItem> *itemView, NSUInteger idx, BOOL * _Nonnull stop) {
        
+        CGFloat width = WIDTH - 30;
+        CGFloat step = (width / 2 - 15) * idx + 15;
+        
+        CGRect itemViewFrame = CGRectMake(step, Y_POSITION, IMAGESIZE, IMAGESIZE);
+        itemView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleTopMargin;
+        itemView.frame = itemViewFrame;
+        if (self.currentPage + 1 == idx) {
+            [self updateItemView:itemView withRation:1.0];
+        } else {
+            [self updateItemView:itemView withRation:0.0];
+        }
+
     }];
 }
 - (void)tapGestureHandle:(UITapGestureRecognizer *)tapGesture {
     NSInteger pageIndex = [self.itemViews indexOfObject:tapGesture.view];
     [self.navigationController setCurrentPage:pageIndex animated:YES];
+}
+#pragma mark - Other
+- (void)updateItemView:(UIView<DDTinderNavigationBarItem> *)itemView withRation:(CGFloat)ration {
+    if ([itemView respondsToSelector:@selector(updateViewWithRatio:)]) {
+        [itemView updateViewWithRatio:ration];
+    }
+}
+#pragma mark - Properties
+- (void)setContentOffSet:(CGPoint)contentOffSet {
+    _contentOffSet = contentOffSet;
+    
+    CGFloat xOffset = contentOffSet.x;
+    
+    CGFloat normalWidth = CGRectGetWidth([[UIScreen mainScreen] bounds]);
+    
+    [self.itemViews enumerateObjectsUsingBlock:^(UIView<DDTinderNavigationBarItem> *itemView, NSUInteger idx, BOOL * _Nonnull stop) {
+        
+        CGFloat width = WIDTH - 30;
+        CGFloat step = (width / 2 - 15) * idx + 15;
+        
+        
+    }];
+    
 }
 - (void)setItemViews:(NSArray *)itemViews {
     if (itemViews) {
@@ -39,6 +79,7 @@
             [self addSubview:itemView];
         }];
     }
+    _itemViews = itemViews;
 }
 #pragma mark - Lift Cycle
 - (instancetype)initWithFrame:(CGRect)frame
