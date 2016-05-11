f63a366e9174cc03298ead934349ee0c83897fe9
张德荣 <zdrjson@sina.com>
2016-05-11 18:13:03 +0800

    add radian and degree exchange

--- Tinder/DDCGUtilities.h
+++ Tinder/DDCGUtilities.h
@@ -14,3 +14,11 @@ CGFloat DDScreenScale();
 /// Get main screen's size. Height is always larger than width.
 CGSize DDScreenSize();
 
+/// Convert degrees to radians.
+static inline CGFloat DegreesToRadians(CGFloat degrees) {
+    return degrees * M_PI / 180;
+}
+/// Convert radians to degrees.
+static inline CGFloat RadiansToDegrees(CGFloat radians) {
+    return radians * 180 / M_PI;
+}
