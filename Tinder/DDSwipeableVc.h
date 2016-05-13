55cd135574ffdb439169cd55de6ac5dcf85cc0b0
张德荣 <zdrjson@sina.com>
2016-05-13 12:35:55 +0800

    add swipeableVc

---
+++ Tinder/DDSwipeableVc.h
@@ -0,0 +1,13 @@
+//
+//  DDSwipeableVc.h
+//  Tinder
+//
+//  Created by 张德荣 on 16/5/13.
+//  Copyright © 2016年 JsonZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDSwipeableVc : UIViewController
+
+@end
