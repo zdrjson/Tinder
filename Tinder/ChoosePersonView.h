7d7cc7e18632bc3d104aaad5cb519c35b9526670
张德荣 <zdrjson@sina.com>
2016-05-17 12:08:54 +0800

    add ChoosePersionView

---
+++ Tinder/ChoosePersonView.h
@@ -0,0 +1,13 @@
+//
+//  ChoosePersonView.h
+//  Tinder
+//
+//  Created by 张德荣 on 16/5/17.
+//  Copyright © 2016年 JsonZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MDCSwipeToChoose/MDCSwipeToChoose.h>
+@interface ChoosePersonView : MDCSwipeToChooseView
+
+@end
---
+++ Tinder/ChoosePersonView.m
@@ -0,0 +1,21 @@
+//
+//  ChoosePersonView.m
+//  Tinder
+//
+//  Created by 张德荣 on 16/5/17.
+//  Copyright © 2016年 JsonZhang. All rights reserved.
+//
+
+#import "ChoosePersonView.h"
+
+@implementation ChoosePersonView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
