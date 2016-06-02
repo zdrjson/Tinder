0d8a6476dee8a744c57a0253ff828736b9f3880e
张德荣 <zdrjson@gmail.com>
2016-06-02 18:06:50 +0800

    add ddapiProxy class

---
+++ Tinder/DDApiProxy.h
@@ -0,0 +1,13 @@
+//
+//  DDApiProxy.h
+//  Tinder
+//
+//  Created by 张德荣 on 16/6/2.
+//  Copyright © 2016年 JsonZhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DDApiProxy : NSObject
+
+@end
---
+++ Tinder/DDApiProxy.m
@@ -0,0 +1,13 @@
+//
+//  DDApiProxy.m
+//  Tinder
+//
+//  Created by 张德荣 on 16/6/2.
+//  Copyright © 2016年 JsonZhang. All rights reserved.
+//
+
+#import "DDApiProxy.h"
+
+@implementation DDApiProxy
+
+@end
