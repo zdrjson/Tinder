f917262e3ed61be5739aba0cd9c2c133270c2d66
张德荣 <zdrjson@gmail.com>
2016-06-14 17:54:09 +0800

    add ddservice @property

--- Tinder/DDService.h
+++ Tinder/DDService.h
@@ -8,6 +8,34 @@
 
 #import <Foundation/Foundation.h>
 
+//所有DDService的派生类都要符合这个protocal
+@protocol DDServiceProtocal <NSObject>
+
+@property (nonatomic, readonly) BOOL isOnline;
+
+@property (nonatomic, readonly) NSString *offlineApiBaseUrl;
+@property (nonatomic, readonly) NSString *onlineApiBaseUrl;
+
+@property (nonatomic, readonly) NSString *offlineApiVersion;
+@property (nonatomic, readonly) NSString *onflineApiVersion;
+
+@property (nonatomic, readonly) NSString *onlinePublicKey;
+@property (nonatomic, readonly) NSString *offlinePublicKey;
+
+@property (nonatomic, readonly) NSString *onlinePrivateKey;
+@property (nonatomic, readonly) NSString *offlinePrivateKey;
+
+
+@end
+
 @interface DDService : NSObject
+
+
+@property (nonatomic, strong, readonly) NSString *publicKey;
+@property (nonatomic, strong, readonly) NSString *privateKey;
+@property (nonatomic, strong, readonly) NSString *apiBaseUrl;
+@property (nonatomic, strong, readonly) NSString *apiVersion;
+
+@property (nonatomic, weak) id <DDServiceProtocal> child;
 
 @end
